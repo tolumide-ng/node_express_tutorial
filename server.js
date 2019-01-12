c7f606ad777b2e100a1c7af8281300530b452057
Tolumide <tolumideshopein@gmail.com>
2019-01-12 13:59:50 +0100

    Routes with data-structures

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import Reflection from './src/controllers/Reflection';
 
 const app = express();
 
 app.use(express.json());
+
+app.post('/api/v1/reflections', Reflection.create);
+app.get('/api/v1/reflections', Reflection.getAll);
+app.get('/api/v1/reflections/:id', Reflection.getOne);
+app.put('/api/v1/reflections/:id', Reflection.update);
+app.delete('/api/v1/reflections/:id', Reflection.delete);
 
 app.get('/', (req, res) => res.status(200).send({ message: 'YAY! Congratulations' }));
 
