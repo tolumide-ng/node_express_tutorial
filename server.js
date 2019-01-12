4a80c4ca9ad15701fd8f0060d2214c23e1f4a3dc
Tolumide <tolumideshopein@gmail.com>
2019-01-12 14:33:36 +0100

    changes to server.js

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => res.status(200).send({ message: 'YAY! Congratulations' }));
+
 app.post('/api/v1/reflections', Reflection.create);
 app.get('/api/v1/reflections', Reflection.getAll);
 app.get('/api/v1/reflections/:id', Reflection.getOne);
 app.put('/api/v1/reflections/:id', Reflection.update);
 app.delete('/api/v1/reflections/:id', Reflection.delete);
 
-app.get('/', (req, res) => res.status(200).send({ message: 'YAY! Congratulations' }));
 
 const port = process.env.PORT || 3000;
 console.log(`app is running on port ${port}`);
